11c63994840f321686f7e07183f2354a8f2a9e63
linamarcela <linacolorado03@hotmail.com>
2015-09-26 13:40:59 -0500

    se agrega el mensaje del chat

--- js/chat/chat.js
+++ js/chat/chat.js
@@ -1,15 +1,23 @@
 $(function(){
-	var objFirebase = new Firebase("https://chatucc.firebaseio.com/");
+	var objFirebase = new Firebase("https://chatucc2.firebaseio.com/");
+	var contador = 0;
 
 	$('input[type=submit]').click(clickEnvio);
 
 	function clickEnvio(){		
 		var mensaje = $('#inMensaje').val();
 		$('#inMensaje').val('');
-		
-		objFirebase.push({  			
-  			autor: "Jggomezt",
-  			mensaje: mensaje
+		var autor = "";
+		if(contador == "0"){
+			autor = "lmcolorado";
+		}
+		else{
+			autor = "jsusa";
+		}
+		objFirebase.push({ 
+				autor: autor,
+  				mensaje: mensaje
+  			
 		});
 
 		console.log(mensaje);
@@ -17,14 +25,21 @@ $(function(){
 
 	objFirebase.on("child_added", function(data){
 		var registro = data.val();
-		$('.cont-mensajes-timeline').prepend(getPlantilla(registro.autor,registro.mensaje));
+		$('.cont-mensajes-timeline').append(getPlantilla(registro.autor,registro.mensaje));
 
 	});
 
 	function getPlantilla(autor,mensaje){
-		var plantilla = '<div class="cont-mensajes-mensaje"><label for="" id="lblMensaje">'+ autor +'-->'+mensaje+'</label></div>'
-		$('.cont-mensajes-timeline').prepend(plantilla);
-		$('#inMensaje').val('');
+		if(autor == "lmcolorado"){
+			var plantilla = '<div class="section-avatar "><figure><img src="../../img/logo2.png" alt="Lina colorado"></figure></div><div><label for="" id="lblMensaje">'+ autor +'</label></div><div class="me_textarea_envia"><textarea>'+'  '+mensaje+'</textarea></div>';
+			contador = 1;
+		}
+		else{
+			var plantilla = '<div class=" section-avatar2 div_recibe"><figure><img src="../../img/nina.jpg" alt="Lina colorado"></figure></div><div><label for="" id="lblMensaje" class="div_recibe">'+ autor +'</label></div><div class="me_textarea_recibe div_recibe"><textarea>'+'  '+mensaje+'</textarea></div>';
+			contador = 0;
+		}		
+		//$('.cont-mensajes-timeline').prepend(plantilla);
+		$('#inMensaje').val('');		
 		return plantilla;
 	}
 
